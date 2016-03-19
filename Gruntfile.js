c2c22ae2c326cd1cb532be7db3ae659eb9d29dca
Jayant Bhawal <bhawal.jayant@gmail.com>
2016-03-20 00:50:24 +0530

    Grunted more stuff.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,8 +11,30 @@ module.exports = function (grunt) {
                     livereload: true
                 }
             }
+        },
+        open : {
+            dev: {
+                path: 'http://localhost:8080/'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-open');
+
+    grunt.registerTask('install', 'install npm and bower dependencies', function() {
+        var exec = require('child_process').exec;
+        var cb = this.async();
+        exec('npm install', {cwd: '/'}, function(err, stdout, stderr) {
+            console.log(stdout);
+            cb();
+        });
+        exec('bower install', {cwd: '/'}, function(err, stdout, stderr) {
+            console.log(stdout);
+            cb();
+        });
+    });
+
+    grunt.registerTask('default', ['open', 'watch']);
+    grunt.registerTask('setup', ['install', 'default']);
 };
