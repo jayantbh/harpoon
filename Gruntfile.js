1b2939d030c311e3762ebaba71292426067c600a
Jayant Bhawal <bhawal.jayant@gmail.com>
2016-03-20 01:13:21 +0530

    Added more grunt stuff.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,15 +4,32 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        watch: {
-            assets: {
-                files: ['**/*.css','**/*.js','**/*.html'],
+        express: {
+            options: {
+                port: process.env.port || 8080
+            },
+            dev: {
                 options: {
-                    livereload: true
+                    script: 'index.js'
                 }
             }
         },
-        open : {
+        watch: {
+            assets: {
+                files: ['**/*.css', '**/*.js', '**/*.html'],
+                options: {
+                    livereload: true
+                }
+            },
+            express: {
+                files:  [ '**/*.js' ],
+                tasks:  [ 'express:dev' ],
+                options: {
+                    spawn: false
+                }
+            }
+        },
+        open: {
             dev: {
                 path: 'http://localhost:8080/'
             }
@@ -21,20 +38,30 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-express-server');
 
-    grunt.registerTask('install', 'install npm and bower dependencies', function() {
+    grunt.registerTask('install', 'install npm and bower dependencies', function () {
         var exec = require('child_process').exec;
         var cb = this.async();
-        exec('npm install', {cwd: '/'}, function(err, stdout, stderr) {
+        exec('npm install', {cwd: '/'}, function (err, stdout, stderr) {
             console.log(stdout);
             cb();
         });
-        exec('bower install', {cwd: '/'}, function(err, stdout, stderr) {
+        exec('bower install', {cwd: '/'}, function (err, stdout, stderr) {
+            console.log(stdout);
+            cb();
+        });
+    });
+    grunt.registerTask('serve', 'launch server', function () {
+        var exec = require('child_process').exec;
+        var cb = this.async();
+        exec('npm start', {cwd: '/'}, function (err, stdout, stderr) {
             console.log(stdout);
             cb();
         });
     });
 
-    grunt.registerTask('default', ['open', 'watch']);
+    grunt.registerTask('server', [ 'express:dev' ]);
+    grunt.registerTask('default', ['server', 'open', 'watch']);
     grunt.registerTask('setup', ['install', 'default']);
 };
