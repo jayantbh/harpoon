1aa73e2360c29fa10644e7f1a97a0a6eba43c4d1
Jayant Bhawal <bhawal.jayant@gmail.com>
2016-03-25 17:40:46 +0530

    Allowed all CORS headers.

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ var port = process.env.PORT || 8080;
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
