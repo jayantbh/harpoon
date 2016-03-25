46e1201a8af427ffa6832298838b5737921b251b
Jayant Bhawal <bhawal.jayant@gmail.com>
2016-03-25 17:43:10 +0530

    Minor change.

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ var port = process.env.PORT || 8080;
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
+  // res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
